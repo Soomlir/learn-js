2f31bd73eb820504bd84c48702e7bd6dde328cc7
Soomlir <Soomlir@yandex.ru>
2024-06-23 14:31:45 +0300

    1lvl tasks add

---
+++ basic-tasks/strings/01.js
@@ -0,0 +1,37 @@
+// 1. Дана строка. Выведите в консоль длину этой строки. 
+// const greeting = 'Hello World';
+// console.log(greeting.length);
+
+// 2. Дана строка. Выведите в консоль последний символ строки. 
+// const message = 'How are you?';
+// console.log(message[message.length - 1]);
+
+// 3. Даны два слова. Проверьте, что первые буквы этих слов совпадают.
+// const word = 'Apple';
+// const word2 = 'Auto';
+
+// if (word[0] === word2[0]) {
+//   console.log(true);
+// }
+
+// 4. Дано слово. Получите его последнюю букву. 
+// Если слово заканчивается на мягкий знак, то получите предпоследнюю букву.
+// const word3 = 'Мягковь';
+// if (word3[word3.length - 1] === 'ь') {
+//   console.log(word3[word3.length - 2]);
+// } else {
+//   console.log(word3[word3.length - 1]);
+// }
+
+// 5. Дана строка. Если в этой строке более одного символа, 
+// выведите в консоль предпоследний символ этой строки.
+// const word4 = 'Banana';
+// if (word4.length > 1) {
+//   console.log(word4[word4.length - 2]);
+// }
+
+// 6. Дана некоторая строка. Переберите и выведите в консоль по очереди все символы с конца строки.
+// const message2 = 'Applesound';
+// for (let i = message2.length - 1; i >= 0; i--) {
+//   console.log(message2[i]);
+// }
