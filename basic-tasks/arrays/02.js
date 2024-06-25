dcb3a5c7626013d5781c31159bef92094d63c917
Soomlir <Soomlir@yandex.ru>
2024-06-25 19:06:37 +0300

    2lvl new tasks

---
+++ basic-tasks/arrays/02.js
@@ -0,0 +1,8 @@
+// 1. Выведите в консоль все числа в промежутке от 1 до 1000, сумма первой и второй цифры которых равна пяти. 
+for (let i = 10; i < 1000; i++) {
+  const firstDigit = Math.floor(i / 10);
+  const secondDigit = i % 10;
+  if (firstDigit + secondDigit === 5) {
+    console.log(i);
+  }
+}
---
+++ basic-tasks/strings/02.js
@@ -0,0 +1,6 @@
+// 1. Дана некоторая строка. Найдите позицию первого нуля в строке.
+const string = 'Apple0Banana0';
+const position = string.indexOf('0');
+
+console.log(position); // Выведет 5
+
